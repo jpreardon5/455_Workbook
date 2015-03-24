56cb0eb498daea61896ba568d9d1018d3677df0e
jpreardon5 <jamespatrickreardon@gmail.com>
2015-03-23 23:32:40 -0400

    Update motherboard_data.sql

--- motherboard_data.sql
+++ motherboard_data.sql
@@ -12,3 +12,44 @@ Insert INTO project_motherboard Values
 (326841, 'mother10', 'Sabre', 299.99, 'Desktop', 'AM3+', 4, 'DDR3', 2133, 'INTEL', 'MATX', 2, 1, 1, 2, 4, 1, '24 Pin', '4 Pin', 4, 1, 1, 1, 1,'Y', null),
 (36269, 'mother11', 'Sabre', 299.99, 'Desktop', 'LGA1150', 4, 'DDR3', 2133, 'AMD', 'ATX', 2, 2, 1, 2, 4, 1, '24 Pin', '8 Pin', 4, 1, 1, 1, 1,'Y', null),
 (14588, 'mother12', 'DustbInc', 299.99, 'Desktop', 'AM3+', 4, 'DDR3', 2133, 'INTEL', 'ATX', 2, 1, 1, 2, 4, 1, '24 Pin', '8 Pin', 4, 1, 1, 1, 2,'Y', null);
+
+
+insert into project_user values('opticaldrive0', 'uniquename0', 'uniquebrand0', 19.99, 'DVD', 'SATA', 'Tray');
+
+
+
+
+insert into project_user values
+('user_id0', 'username0', '2008-01-01 00:00:01', '0000-00-00 00:00:00', 'uniqueemail0', 'salt0', 'password0'),
+('user_id1', 'username1', '2008-01-01 00:00:01', '0000-00-00 00:00:00', 'uniqueemail1', 'salt1', 'password1'),
+('user_id2', 'username2', '2008-01-01 00:00:01', '0000-00-00 00:00:00', 'uniqueemail2', 'salt2', 'password2'),
+('user_id3', 'username3', '2008-01-01 00:00:01', '0000-00-00 00:00:00', 'uniqueemail3', 'salt3', 'password3'),
+('user_id4', 'username4', '2008-01-01 00:00:01', '0000-00-00 00:00:00', 'uniqueemail4', 'salt4', 'password4'),
+('user_id5', 'username5', '2008-01-01 00:00:01', '0000-00-00 00:00:00', 'uniqueemail5', 'salt5', 'password5'),
+('user_id6', 'username6', '2008-01-01 00:00:01', '0000-00-00 00:00:00', 'uniqueemail6', 'salt6', 'password6'),
+('user_id7', 'username7', '2008-01-01 00:00:01', '0000-00-00 00:00:00', 'uniqueemail7', 'salt7', 'password7'),
+('user_id8', 'username8', '2008-01-01 00:00:01', '0000-00-00 00:00:00', 'uniqueemail8', 'salt8', 'password8'),
+('user_id9', 'username9', '2008-01-01 00:00:01', '0000-00-00 00:00:00', 'uniqueemail9', 'salt9', 'password9');
+
+
+
+
+insert into project_memory values
+('memory0', 'uniquename0', 'uniquebrand0', 159.99, '240 pin DIMM', 8192, 2, 'DDR3'),
+('memory1', 'uniquename1', 'uniquebrand1', 159.99, '240 pin DIMM', 8192, 2, 'DDR3'),
+('memory2', 'uniquename2', 'uniquebrand2', 159.99, '240 pin DIMM', 8192, 2, 'DDR3'),
+('memory3', 'uniquename3', 'uniquebrand3', 159.99, '240 pin DIMM', 8192, 2, 'DDR3'),
+('memory4', 'uniquename4', 'uniquebrand4', 159.99, '240 pin DIMM', 8192, 2, 'DDR3'),
+('memory5', 'uniquename5', 'uniquebrand5', 159.99, '240 pin DIMM', 8192, 2, 'DDR3'),
+('memory6', 'uniquename6', 'uniquebrand6', 159.99, '240 pin DIMM', 8192, 2, 'DDR3'),
+('memory7', 'uniquename7', 'uniquebrand7', 159.99, '240 pin DIMM', 8192, 2, 'DDR3'),
+('memory8', 'uniquename8', 'uniquebrand8', 159.99, '240 pin DIMM', 8192, 2, 'DDR3'),
+('memory9', 'uniquename9', 'uniquebrand9', 159.99, '240 pin DIMM', 8192, 2, 'DDR3');
+
+insert into project_case values
+('case0', 'uniquename0', 'uniquebrand0', 149.99, 'aluminum', 'ATX', 2, 1),
+('case0', 'uniquename0', 'uniquebrand0', 149.99, 'aluminum', 'ATX', 2, 1),
+('case0', 'uniquename0', 'uniquebrand0', 149.99, 'aluminum', 'ATX', 2, 1),
+('case0', 'uniquename0', 'uniquebrand0', 149.99, 'aluminum', 'ATX', 2, 1),
+('case0', 'uniquename0', 'uniquebrand0', 149.99, 'aluminum', 'ATX', 2, 1),
+('case0', 'uniquename0', 'uniquebrand0', 149.99, 'aluminum', 'ATX', 2, 1),

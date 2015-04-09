9aff1254a23427d9bd3153cc8a4b0e0eecb37bfe
jpreardon5 <jamespatrickreardon@gmail.com>
2015-04-09 00:42:36 -0400

    Update data_fresh.sql

--- data_fresh.sql
+++ data_fresh.sql
@@ -1,1 +1,23 @@
 
+UPDATE project_case SET name = "Corsair" WHERE model_number = "case0 ";
+UPDATE project_case SET name = "Stryker " WHERE model_number = "case1 ";
+UPDATE project_case SET name = "Rosewill " WHERE model_number = "case2 ";
+UPDATE project_case SET name = "Cooler_Master" WHERE model_number = "case3 ";
+UPDATE project_case SET name = "DeepCool" WHERE model_number = "case4 ";
+UPDATE project_case SET name = "Phantom" WHERE model_number = "case5 ";
+UPDATE project_case SET name = "Silverstone" WHERE model_number = "case6 ";
+UPDATE project_case SET name = "Phanteks" WHERE model_number = "case7 ";
+UPDATE project_case SET name = "Fractal" WHERE model_number = "case8 ";
+UPDATE project_case SET name = "Hadron" WHERE model_number = "case9 ";
+UPDATE project_case SET brand = "LogiCase" WHERE model_number = "case0 ";
+UPDATE project_case SET brand = "Cougar " WHERE model_number = "case1 ";
+UPDATE project_case SET brand = "Raidmax " WHERE model_number = "case2 ";
+UPDATE project_case SET brand = "LogiCase" WHERE model_number = "case3 ";
+UPDATE project_case SET brand = "Cougar " WHERE model_number = "case4 ";
+UPDATE project_case SET brand = "LogiCase" WHERE model_number = "case5 ";
+UPDATE project_case SET brand = "Raidmax " WHERE model_number = "case6 ";
+UPDATE project_case SET brand = "Supermicro " WHERE model_number = "case7 ";
+UPDATE project_case SET brand = "LogiCase" WHERE model_number = "case8 ";
+UPDATE project_case SET brand = "Supermicro " WHERE model_number = "case9 ";
+
+

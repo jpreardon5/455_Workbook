42f2164dc428f6383938c4c520450f1a8a70fd6b
jpreardon5 <jamespatrickreardon@gmail.com>
2015-04-09 02:18:01 -0400

    Update data_fresh.sql

--- data_fresh.sql
+++ data_fresh.sql
@@ -20,6 +20,14 @@ UPDATE project_case SET brand = "Supermicro " WHERE model_number = "case7 ";
 UPDATE project_case SET brand = "LogiCase" WHERE model_number = "case8 ";
 UPDATE project_case SET brand = "Supermicro " WHERE model_number = "case9 ";
 
+UPDATE project_case SET price = "139.99" WHERE model_number = "case0 ";
+UPDATE project_case SET price = "128.99" WHERE model_number = "case2 ";
+UPDATE project_case SET price = "249.99" WHERE model_number = "case4 ";
+UPDATE project_case SET price = "225.99" WHERE model_number = "case6 ";
+UPDATE project_case SET price = "199.99" WHERE model_number = "case8 ";
+UPDATE project_case SET price = "299.99" WHERE model_number = "case9 ";
+
+
 
 UPDATE project_cpu SET name = "FX-6300" WHERE model_number = "cpu0 ";
 UPDATE project_cpu SET name = "i7-4790" WHERE model_number = "cpu1 ";
@@ -31,6 +39,13 @@ UPDATE project_cpu SET name = "FX-4350" WHERE model_number = "cpu6 ";
 UPDATE project_cpu SET name = "Corei7-4130" WHERE model_number = "cpu7 ";
 UPDATE project_cpu SET name = "XeonE5-2699" WHERE model_number = "cpu8 ";
 UPDATE project_cpu SET name = "A8-7600" WHERE model_number = "cpu9 ";
+
+UPDATE project_cpu SET price = "189.99" WHERE model_number = "cpu0 ";
+UPDATE project_cpu SET price = "360.99" WHERE model_number = "cpu2 ";
+UPDATE project_cpu SET price = "289.99" WHERE model_number = "cpu4 ";
+UPDATE project_cpu SET price = "258.99" WHERE model_number = "cpu6 ";
+UPDATE project_cpu SET price = "325.99" WHERE model_number = "cpu8 ";
+UPDATE project_cpu SET price = "199.99" WHERE model_number = "cpu9 ";
 
 UPDATE project_cpu SET brand = "AMD" WHERE model_number = "cpu0 ";
 UPDATE project_cpu SET brand = "Intel" WHERE model_number = "cpu1 ";
@@ -103,45 +118,120 @@ UPDATE project_displays SET dvi_ports = " " WHERE model_number = "displays6";
 UPDATE project_displays SET dvi_ports = " " WHERE model_number = "displays8";
 
 
-UPDATE project_memory SET name = " " WHERE model_number = " ";
-UPDATE project_memory SET name = " " WHERE model_number = " ";
-UPDATE project_memory SET name = " " WHERE model_number = " ";
-UPDATE project_memory SET name = " " WHERE model_number = " ";
-UPDATE project_memory SET name = " " WHERE model_number = " ";
-UPDATE project_memory SET name = " " WHERE model_number = " ";
-UPDATE project_memory SET name = " " WHERE model_number = " ";
-UPDATE project_memory SET name = " " WHERE model_number = " ";
-UPDATE project_memory SET name = " " WHERE model_number = " ";
-UPDATE project_memory SET name = " " WHERE model_number = " ";
+UPDATE project_memory SET name = "Radeon R5" WHERE model_number = "memory0";
+UPDATE project_memory SET name = "Vengence" WHERE model_number = "memory1";
+UPDATE project_memory SET name = "Radeon R3" WHERE model_number = "memory2";
+UPDATE project_memory SET name = "Dominator" WHERE model_number = "memory3";
+UPDATE project_memory SET name = "Cras" WHERE model_number = "memory4";
+UPDATE project_memory SET name = "Genuine" WHERE model_number = "memory5";
+UPDATE project_memory SET name = "Performance" WHERE model_number = "memory6";
+UPDATE project_memory SET name = "Viper 3" WHERE model_number = "memory7";
+UPDATE project_memory SET name = "Ripjaws" WHERE model_number = "memory8";
+UPDATE project_memory SET name = "Blackline" WHERE model_number = "memory9";
 
-UPDATE project_memory SET brand = " " WHERE model_number = " ";
-UPDATE project_memory SET brand = " " WHERE model_number = " ";
-UPDATE project_memory SET brand = " " WHERE model_number = " ";
-UPDATE project_memory SET brand = " " WHERE model_number = " ";
-UPDATE project_memory SET brand = " " WHERE model_number = " ";
-UPDATE project_memory SET brand = " " WHERE model_number = " ";
-UPDATE project_memory SET brand = " " WHERE model_number = " ";
-UPDATE project_memory SET brand = " " WHERE model_number = " ";
-UPDATE project_memory SET brand = " " WHERE model_number = " ";
+UPDATE project_memory SET brand = "AMD" WHERE model_number = "memory0";
+UPDATE project_memory SET brand = "Corsair" WHERE model_number = "memory1";
+UPDATE project_memory SET brand = "AMD" WHERE model_number = "memory2";
+UPDATE project_memory SET brand = "Corsair" WHERE model_number = "memory3";
+UPDATE project_memory SET brand = "Klevv" WHERE model_number = "memory4";
+UPDATE project_memory SET brand = "Klevv" WHERE model_number = "memory5";
+UPDATE project_memory SET brand = "AMD" WHERE model_number = "memory6";
+UPDATE project_memory SET brand = "Patriot" WHERE model_number = "memory7";
+UPDATE project_memory SET brand = "G.Skill" WHERE model_number = "memory8";
 
-UPDATE project_memory SET memory_type = " " WHERE model_number = " ";
-UPDATE project_memory SET memory_type = " " WHERE model_number = " ";
-UPDATE project_memory SET memory_type = " " WHERE model_number = " ";
-UPDATE project_memory SET memory_type = " " WHERE model_number = " ";
-UPDATE project_memory SET memory_type = " " WHERE model_number = " ";
-UPDATE project_memory SET memory_type = " " WHERE model_number = " ";
+UPDATE project_memory SET memory_type = "240 pin DIMM" WHERE model_number = "memory0";
+UPDATE project_memory SET memory_type = "240 pin DIMM" WHERE model_number = "memory2";
+UPDATE project_memory SET memory_type = "288 pin DIMM" WHERE model_number = "memory4";
+UPDATE project_memory SET memory_type = "240 pin DIMM" WHERE model_number = "memory6";
+UPDATE project_memory SET memory_type = "288 pin DIMM" WHERE model_number = "memory8";
+UPDATE project_memory SET memory_type = "288 pin DIMM" WHERE model_number = "memory9";
 
-UPDATE project_memory SET size_per_stick_megabytes = " " WHERE model_number = " ";
-UPDATE project_memory SET size_per_stick_megabytes = " " WHERE model_number = " ";
-UPDATE project_memory SET size_per_stick_megabytes = " " WHERE model_number = " ";
-UPDATE project_memory SET size_per_stick_megabytes = " " WHERE model_number = " ";
+UPDATE project_memory SET size_per_stick_megabytes = " " WHERE model_number = "memory2";
+UPDATE project_memory SET size_per_stick_megabytes = " " WHERE model_number = "memory4";
+UPDATE project_memory SET size_per_stick_megabytes = " " WHERE model_number = "memory6";
+UPDATE project_memory SET size_per_stick_megabytes = " " WHERE model_number = "memory8";
 
-
-UPDATE project_memory SET price = " " WHERE model_number = " ";
+UPDATE project_memory SET speed = "DDR4" WHERE model_number = "memory0";
+UPDATE project_memory SET speed = "DDR4" WHERE model_number = "memory2";
+UPDATE project_memory SET speed = "DDR4" WHERE model_number = "memory4";
+UPDATE project_memory SET speed = "DDR4"  WHERE model_number = "memory6";
+UPDATE project_memory SET speed = "DDR4" WHERE model_number = "memory8";
 
 
 UPDATE project_memory SET number_of_sticks = " " WHERE model_number = " ";
-UPDATE project_memory SET speed = " " WHERE model_number = " ";
+
+UPDATE project_memory SET price = "189.99" WHERE model_number = "memory0";
+UPDATE project_memory SET price = "264.99" WHERE model_number = "memory2";
+UPDATE project_memory SET price = "178.99" WHERE model_number = "memory4";
+UPDATE project_memory SET price = "289.99" WHERE model_number = "memory6";
+UPDATE project_memory SET price = "195.99" WHERE model_number = "memory8";
+
+UPDATE project_optical_drive SET model_number = " " WHERE model_number = " ";
+
+UPDATE project_optical_drive SET name = "DDU1681" WHERE model_number = "opticaldrive0";
+UPDATE project_optical_drive SET name = "BDR-206" WHERE model_number = "opticaldrive1";
+UPDATE project_optical_drive SET name = "SOD02" WHERE model_number = "opticaldrive2";
+UPDATE project_optical_drive SET name = "GH24NS" WHERE model_number = "opticaldrive3";
+UPDATE project_optical_drive SET name = "AD-7260" WHERE model_number = "opticaldrive4";
+UPDATE project_optical_drive SET name = "WH12L" WHERE model_number = "opticaldrive5";
+UPDATE project_optical_drive SET name = "BW-16D" WHERE model_number = "opticaldrive6";
+UPDATE project_optical_drive SET name = "CB-5021" WHERE model_number = "opticaldrive7";
+UPDATE project_optical_drive SET name = "AD-7740" WHERE model_number = "opticaldrive8";
+UPDATE project_optical_drive SET name = "L89OSA" WHERE model_number = "opticaldrive9";
+
+UPDATE project_optical_drive SET brand = "Sony" WHERE model_number = "opticaldrive0";
+UPDATE project_optical_drive SET brand = "Pioneer" WHERE model_number = "opticaldrive1";
+UPDATE project_optical_drive SET brand = "Silverstone" WHERE model_number = "opticaldrive2";
+UPDATE project_optical_drive SET brand = "LG" WHERE model_number = "opticaldrive3";
+UPDATE project_optical_drive SET brand = "Sony" WHERE model_number = "opticaldrive4";
+UPDATE project_optical_drive SET brand = "LG" WHERE model_number = "opticaldrive5";
+UPDATE project_optical_drive SET brand = "Asus " WHERE model_number = "opticaldrive6";
+UPDATE project_optical_drive SET brand = "Archgon" WHERE model_number = "opticaldrive7";
+UPDATE project_optical_drive SET brand = "Sony" WHERE model_number = "opticaldrive8";
+UPDATE project_optical_drive SET brand = "Plextor" WHERE model_number = "opticaldrive9";
+
+UPDATE project_optical_drive SET price = "79.99" WHERE model_number = "opticaldrive0";
+UPDATE project_optical_drive SET price = "49.99" WHERE model_number = "opticaldrive2";
+UPDATE project_optical_drive SET price = "38.99" WHERE model_number = "opticaldrive4";
+UPDATE project_optical_drive SET price = "64.99" WHERE model_number = "opticaldrive6";
+UPDATE project_optical_drive SET price = "55.99" WHERE model_number = "opticaldrive8";
+UPDATE project_optical_drive SET price = "48.99" WHERE model_number = "opticaldrive9";
+
+
+UPDATE project_optical_drive SET drive_type = " " WHERE model_number = " ";
+UPDATE project_optical_drive SET interface_type = " " WHERE model_number = " ";
+UPDATE project_optical_drive SET load_type = " " WHERE model_number = " ";
+
+UPDATE project_psu SET model_number = " " WHERE model_number = " ";
+
+UPDATE project_psu SET name = " " WHERE model_number = " ";
+UPDATE project_psu SET name = " " WHERE model_number = " ";
+UPDATE project_psu SET name = " " WHERE model_number = " ";
+UPDATE project_psu SET name = " " WHERE model_number = " ";
+UPDATE project_psu SET name = " " WHERE model_number = " ";
+UPDATE project_psu SET name = " " WHERE model_number = " ";
+UPDATE project_psu SET name = " " WHERE model_number = " ";
+UPDATE project_psu SET name = " " WHERE model_number = " ";
+UPDATE project_psu SET name = " " WHERE model_number = " ";
+UPDATE project_psu SET name = " " WHERE model_number = " ";
+
+UPDATE project_psu SET brand = " " WHERE model_number = " ";
+UPDATE project_psu SET brand = " " WHERE model_number = " ";
+UPDATE project_psu SET brand = " " WHERE model_number = " ";
+UPDATE project_psu SET brand = " " WHERE model_number = " ";
+UPDATE project_psu SET brand = " " WHERE model_number = " ";
+UPDATE project_psu SET brand = " " WHERE model_number = " ";
+UPDATE project_psu SET brand = " " WHERE model_number = " ";
+UPDATE project_psu SET brand = " " WHERE model_number = " ";
+UPDATE project_psu SET brand = " " WHERE model_number = " ";
+UPDATE project_psu SET price = " " WHERE model_number = " ";
+UPDATE project_psu SET wattage = " " WHERE model_number = " ";
+UPDATE project_psu SET modular = " " WHERE model_number = " ";
+UPDATE project_psu SET primary_mobo_pin = " " WHERE model_number = " ";
+UPDATE project_psu SET secondary_mobo_pin = " " WHERE model_number = " ";
+UPDATE project_psu SET sata_power_connectors = " " WHERE model_number = " ";
+UPDATE project_psu SET molex_power_connectors = " " WHERE model_number = " ";
+UPDATE project_psu SET floppy_power_connectors = " " WHERE model_number = " ";
 
 
 

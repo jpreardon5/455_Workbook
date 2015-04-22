51cf743946a1c17ba12960d7f8b2d0e185f78c6c
jpreardon5 <jamespatrickreardon@gmail.com>
2015-04-22 19:45:31 -0400

    Update table_update.sql

--- table_update.sql
+++ table_update.sql
@@ -1,1 +1,12 @@
+drop table if exists project_user;
+drop table if exists project_cart;
 
+create table project_user (
+      username varchar (50) PRIMARY KEY,
+      creation_date timestamp not null default CURRENT_TIMESTAMP,
+      last_login timestamp not null,
+      email varchar (200) not null,
+      salt char (21) not null,
+      password char (60) not null,
+      unique key (email)
+      );
